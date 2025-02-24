fac16a0bb00abeacc7eef9f7b54990f1ee6e6bd6
0xtdlArk <ilyat@arkada.gg>
2025-02-24 11:10:14 +0100

    feat: daily check aggregator script

---
+++ scripts/external/daily-check/helpers/write-txs.ts
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+
+import { DailyCheckEvent } from '../../../../typechain-types/contracts/DailyCheck';
+
+interface ITxData {
+  hash: string;
+  event_name: string;
+  block_number: number;
+  args: string;
+  created_at: string;
+}
+
+export const writeTxs = (events: DailyCheckEvent[]) => {
+  const txs = events.map<ITxData>((event) => ({
+    hash: event.transactionHash,
+    event_name: event.event ?? 'Unknown',
+    block_number: event.blockNumber,
+    args: JSON.stringify(event.args),
+    created_at: new Date(event.args.timestamp.toNumber() * 1000).toISOString(),
+  }));
+
+  const dir = 'scripts-data';
+  const filePath = path.join(dir, 'tx.json');
+
+  // Ensure the directory exists
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+
+  fs.writeFileSync(filePath, JSON.stringify(txs, null, 2), 'utf8');
+};
---
+++ scripts/external/daily-check/index.ts
@@ -0,0 +1,30 @@
+import * as hre from 'hardhat';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+
+import { getAllDailyCheckEvents } from './helpers/get-all-events';
+import { writeTxs } from './helpers/write-txs';
+import { writeUsersComputedData } from './helpers/write-users-computed-data';
+
+const func = async (hre: HardhatRuntimeEnvironment) => {
+  console.log('Getting and sorting events...');
+  const sortedEvents = await getAllDailyCheckEvents(hre);
+  console.log('Events loaded!');
+
+  console.log('--------------------------------------------');
+
+  console.log(
+    'Generating txs records and writing to file (<root>/scripts-data/tx.json)...',
+  );
+  writeTxs(sortedEvents);
+  console.log('Txs recorded!');
+
+  console.log('--------------------------------------------');
+
+  console.log(
+    'Calculating users points and writing to file (<root>/scripts-data/users-points.json)...',
+  );
+  writeUsersComputedData(sortedEvents);
+  console.log('Points recorded!');
+};
+
+func(hre).then(console.log).catch(console.error);
