a4e2c7b2d4bd3b9d7cf8d0ea4688093310905a47
0xtdlArk <ilyat@arkada.gg>
2025-03-03 12:32:24 +0100

    feat: royalty contract to addresses

--- config/constants/addresses.ts
+++ config/constants/addresses.ts
@@ -12,7 +12,7 @@ export const arkadaAddressesPerNetwork: ConfigPerNetwork<
 > = {
   soneium: {
     dailyCheck: '0x98826e728977B25279ad7629134FD0e96bd5A7b2',
-    // arkadaErc721Royalty: '0x98826e728977B25279ad7629134FD0e96bd5A7b2',
+    arkadaErc721Royalty: '0x181b42ca4856237AE76eE8c67F8FF112491eCB9e',
     // arkadaRewarder: '0x98826e728977B25279ad7629134FD0e96bd5A7b2',
   },
   sepolia: undefined,
