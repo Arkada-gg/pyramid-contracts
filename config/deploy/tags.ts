e5ef82358a05080006a93f65fed4979b1bd79348
0xtdlArk <ilyat@arkada.gg>
2025-05-13 11:50:52 +0200

    feat: factory deploy

--- config/deploy/tags.ts
+++ config/deploy/tags.ts
@@ -4,6 +4,9 @@ export const ARKADA_REWARDER_CONTRACT_NAME = 'ArkadaRewarder';
 export const PYRAMID_DEPLOY_TAG = 'Pyramid';
 export const PYRAMID_CONTRACT_NAME = 'Pyramid';
 
+export const FACTORY_DEPLOY_TAG = 'Factory';
+export const FACTORY_CONTRACT_NAME = 'Factory';
+
 export const ARENA_DEPLOY_TAG = 'ArkadaPVPArena';
 export const ARENA_CONTRACT_NAME = 'ArkadaPVPArena';
 
---
+++ scripts/deploy/deploy_EscrowFactory.ts
@@ -0,0 +1,44 @@
+import * as hre from 'hardhat';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+
+import { FACTORY_CONTRACT_NAME } from '../../config';
+import { getCurrentAddresses } from '../../config/constants/addresses';
+import {
+  logDeployProxy,
+  tryEtherscanVerifyImplementation,
+} from '../../helpers/utils';
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const addresses = getCurrentAddresses(hre);
+  const { deployer } = await hre.getNamedAccounts();
+  console.log('deployer', { deployer });
+
+  const owner = await hre.ethers.getSigner(deployer);
+
+  console.log('Deploying Factory...');
+
+  // initialise params <=========
+  const ADMIN = deployer; // Set admin address here
+  const PYRAMID = addresses?.pyramid; // Set arkada rewarder address here
+  // =====================
+
+  const deployment = await hre.upgrades.deployProxy(
+    await hre.ethers.getContractFactory(FACTORY_CONTRACT_NAME, owner),
+    [ADMIN, PYRAMID],
+    {
+      unsafeAllow: ['constructor'],
+    },
+  );
+
+  if (deployment.deployTransaction) {
+    console.log('Waiting 5 blocks...');
+    await deployment.deployTransaction.wait(5);
+    console.log('Waited.');
+  }
+
+  await logDeployProxy(hre, FACTORY_CONTRACT_NAME, deployment.address);
+  await tryEtherscanVerifyImplementation(hre, deployment.address);
+};
+
+func(hre).then(console.log).catch(console.error);
