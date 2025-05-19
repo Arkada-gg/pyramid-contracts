39c80fa253b33a3e1cddb14003535b35bd5fc2c8
Aliaksei <milutkin.work@gmail.com>
2025-05-19 10:29:40 +0300

    traces

--- config/constants/addresses.ts
+++ config/constants/addresses.ts
@@ -5,7 +5,6 @@ export interface ArkadaAddresses {
   arkadaRewarder?: string;
   pyramid?: string;
   arena?: string;
-  escrowFactory?: string;
 }
 
 export const arkadaAddressesPerNetwork: ConfigPerNetwork<
@@ -15,19 +14,16 @@ export const arkadaAddressesPerNetwork: ConfigPerNetwork<
     arkadaRewarder: '0x998F98F806F448cc1eAB301D10f95DE065EE2ABe',
     pyramid: '0x30410050CB1eBCF21741c9D3F817C386401f82fd',
     arena: '',
-    escrowFactory: '',
   },
   sonic: {
     arkadaRewarder: '0xC062fB89Ed30AB70BEf17779A430CE81ccbB8617',
     pyramid: '0xE99F2AEfff9CCff34832747479Bd84658495F50A',
     arena: '',
-    escrowFactory: '',
   },
   base: {
     arkadaRewarder: '0xC062fB89Ed30AB70BEf17779A430CE81ccbB8617',
     pyramid: '0xC909A19E3cE11841d46E9206f5FD9fe2Bc9B36b5',
     arena: '',
-    escrowFactory: '0x2162c19CBC9DDb8b11358e27Ebb901F1e0103088',
   },
   sepolia: undefined,
   hardhat: undefined,
