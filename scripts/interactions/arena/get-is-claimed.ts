819db349b3b42c244c2df703acb0030567e1fcd6
0xtdlArk <ilyat@arkada.gg>
2025-07-21 15:39:38 +0300

    feat: getters

--- scripts/interactions/arena/get-is-claimed.ts
+++ scripts/interactions/arena/get-is-claimed.ts
@@ -2,6 +2,7 @@ import * as hre from 'hardhat';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 
+import { ethers } from 'ethers';
 import { ARENA_CONTRACT_NAME } from '../../../config';
 import { getCurrentAddresses } from '../../../config/constants/addresses';
 
@@ -19,9 +20,17 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   );
 
   const ARENA_ID = 57;
+  const ADDRESS_TO_CHECK = '0x94a9edc38388f1ea1eca824210f28465cca570ce';
 
-  const submittedRoot = await arenaContract.rootProofByArena(ARENA_ID);
-  console.log('Arena submitted root: ', submittedRoot);
+  // Calculate the hash for participant and claimed mappings
+  const arenaIdHash = ethers.utils.solidityKeccak256(['uint256'], [ARENA_ID]);
+  const arenaIdAndAddressHash = ethers.utils.solidityKeccak256(
+    ['bytes32', 'address'],
+    [arenaIdHash, ADDRESS_TO_CHECK],
+  );
+
+  const claimed = await arenaContract.claimed(arenaIdAndAddressHash);
+  console.log('Is claimed: ', claimed);
 };
 
 func(hre).then(console.log).catch(console.error);
